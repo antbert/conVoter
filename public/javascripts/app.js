9b790fd6eae0c56189178e7be20952400ea2b732
Gooz92 <kurguzenkov@gmail.com>
2014-10-17 22:53:36 +0200

    move js libs to separate folder; write app.js

---
+++ public/javascripts/app.js
@@ -0,0 +1,27 @@
+(function () {
+	var convoter = angular.module('convoter', [
+			'ngRoute',
+			'ngResource'
+		]);
+
+	convoter.config(['$routeProvider', function (routeProvider) {
+
+	}]);
+
+	convoter.run(['$resource', function (resource) {
+		
+	}]);
+
+	convoter.controller('HelloController', ['$scope', function (scope) {
+		scope.message = 'hi!';
+	}]);
+	
+	convoter
+		.constant('colorConfig', {
+			colors: []
+		})
+		.constant('I18N.MESSAGES', {
+			'action.vote': 'vote'
+		});
+
+})();
